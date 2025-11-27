73f6c0e013c179fc66e1ff5dd820d512236a7b0e
25kq1a5495-droid <25kq1a5495@pace.ac.in>
2025-11-27 09:01:28 +0530

    Add files via upload

---
+++ function even or odd.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a;
+   void evenodd(int);
+   clrscr();
+   printf("enter a value ");
+   scanf("%d",&a);
+   evenodd(a);
+   getch();
+   }
+   void evenodd(int a)
+   {
+   if(a%2==0)
+   printf("given number is even");
+   else
+   printf("given number is odd");
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+    
