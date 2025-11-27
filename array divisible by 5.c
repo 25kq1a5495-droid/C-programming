01c487e2c38a3fc01208f2ab48d0f342b2894a1d
25kq1a5495-droid <25kq1a5495@pace.ac.in>
2025-11-27 08:38:06 +0530

    Add files via upload

---
+++ array divisible by 5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+   int i,a[5];
+   printf("enter 5 integer numbers to continue:\n ");
+   for(i=0;i<=4;i++)
+   scanf("%d",&a[i]);
+   for(i=0;i<=4;i++)
+   {
+   if(a[i]%5==0)
+   printf("given array is divisible by 5:%d\n",a[i]);
+   }
+   return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
